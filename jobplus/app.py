0d921fefd01a8ceae6f5eb7e0bda2b5f5c314430
Aimee <314552015@qq.com>
2018-05-24 22:00:46 +0800

    create jobplus skeleton

---
+++ jobplus/app.py
@@ -0,0 +1,4 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
---
+++ jobplus/config.py
@@ -0,0 +1,23 @@
+class BaseConfig(object):
+    """ 配置基类 """
+    SECRET_KEY = 'secret key'
+
+class DevelopmentConfig(BaseConfig):
+    """ 开发环境配置 """
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:123@localhost:3306/louplus?charset=utf8'
+
+class ProductionConfig(BaseConfig):
+    pass
+
+class TestingConfig(BaseConfig):
+    """ 测试环境配置 """
+    pass
+
+configs={
+        'development' : DevelopmentConfig,
+        'production': ProductionConfig,
+        'testing': TestingConfig
+        }
+
+
