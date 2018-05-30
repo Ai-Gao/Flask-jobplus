33f02d920b01cbb307b2f224df0dc519726a28a8
Aimee <314552015@qq.com>
2018-05-30 19:54:59 +0800

    achieve base.html

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -7,3 +7,13 @@ front = Blueprint('front', __name__)
 def index():
     return render_template('index.html')
 
+# 首页 登录路由
+@front.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template('login.html')
+
+# 首页 注册路由
+@front.route('/register', methods=['POST', 'GET'])
+def register():
+    return render_template('register.html')
+
